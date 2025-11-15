d2e48e0051c3c48f7980cc6003042b77183054b4
ihor <ihor.bielchenko@gmail.com>
2025-11-15 19:57:12 +0300

    Create module.

--- src/QueueService.ts
+++ src/QueueService.ts
@@ -6,22 +6,25 @@ import {
 } from '@nestjs/common';
 import type { IORedisLike } from 'power-redis';
 import { PowerQueues } from 'power-queues';
-import { NestjsPowerRedis } from 'nestjs-power-redis';
+import { RedisService } from 'nestjs-power-redis';
 import { isFunc } from 'full-utils';
 
 @Injectable()
-export class NestjsPowerQueues extends PowerQueues implements OnModuleInit, OnModuleDestroy {
-	public readonly logger: Logger = new Logger('NestjsPowerQueues');
+export class QueueService extends PowerQueues implements OnModuleInit, OnModuleDestroy {
+	public readonly logger: Logger = new Logger('QueueService');
 	public readonly runOnInit: boolean = false;
+	public redis!: IORedisLike;
 	public abort = new AbortController();
 
 	constructor(
-		public readonly redis: NestjsPowerRedis,
+		public readonly redisService: RedisService,
 	) {
 		super();
 	}
 
 	async onModuleInit() {
+		this.redis = this.redisService.redis;
+
 		await this.loadScripts(this.runOnInit);
 		
 		if (this.runOnInit) {
